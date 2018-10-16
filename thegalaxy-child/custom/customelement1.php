597eb04ba3c5cf726f38cb4c8753c80d93ae352d
Corey Sizemore <coreysizemore@gmail.com>
2018-10-16 13:32:47 -0700

    v2.0.7 Theme Updates

---
+++ thegalaxy-child/custom/customelement1.php
@@ -0,0 +1,59 @@
+<div class="home_featured">
+	
+	<div class="featured_wrapper">
+		
+		<a href="http://www.haggarthomes.com/street-of-dreams/">
+		
+			<div class="featured_item">
+				
+				<h3>Street Of Dreams</h3>
+				
+			</div>
+		
+		</a>
+		
+	</div>
+	
+	<div class="featured_wrapper">
+		
+		<a href="http://www.haggarthomes.com/street-of-dreams/">
+		
+			<div class="featured_item">
+				
+				<h3>Street Of Dreams</h3>
+				
+			</div>
+		
+		</a>
+		
+	</div>
+	
+	<div class="featured_wrapper">
+		
+		<a href="http://www.haggarthomes.com/gallery/">
+		
+			<div class="featured_item">
+				
+				<h3>Quarryview</h3>
+				
+			</div>
+		
+		</a>
+		
+	</div>
+	
+	<div class="featured_wrapper">
+		
+		<a href="http://www.haggarthomes.com/gallery/">
+		
+			<div class="featured_item">
+				
+				<h3>Quarryview</h3>
+				
+			</div>
+		
+		</a>
+		
+	</div>
+	
+</div>
